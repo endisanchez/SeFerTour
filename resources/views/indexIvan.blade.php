a2813043d54415fcab8c13e01501271fe06797aa
ivangonzalez07 <igonzalezga19dw31@ikzubirimanteo.com>
2020-12-04 08:36:35 +0100

    landing-page

---
+++ resources/views/indexIvan.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../resources/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../resources/css/estilosIvan.css">
+    <title>Ivan</title>
+</head>
+<body>
+
+    <p class="text-danger">hola que tal</p>
+    
+</body>
+</html>
