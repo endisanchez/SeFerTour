6aaf112ebc1510c198ffaccece4e14d5712be1d3
Endika <esanchezba1931@zubirimanteo.com>
2020-12-16 08:24:03 +0100

    prueba

---
+++ resources/lang/en/messages.php
@@ -0,0 +1,7 @@
+<?php
+
+//resources/lang/en/messages.php
+
+return [
+    'welcome' => 'Welcome to Nigmacode.com'
+];
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+
+
+Route::get('lang/{lang}', 'LanguageController@swap')->name('lang.swap');
