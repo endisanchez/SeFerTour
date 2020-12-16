3f6d52bdd1e433acc968b9fc195d3ae7e500c684
Endika <esanchezba1931@ikzubirimanteo.com>
2020-12-16 10:48:55 +0100

    interfaz acabada

---
+++ resources/lang/es/welcome.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+
+    'home' => '¡Bienvenido a Styde.Net!',
+
+];
--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,11 @@ Route::get('/', function () {
     return view('index');
 });
 
+
+
+Route::get('lang/{lang}', 'LanguageController@swap')->name('lang.swap');
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
