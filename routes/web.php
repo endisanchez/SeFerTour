0a60a5340dd63b368ce5c232bedd7991897d5fb1
Endika <esanchezba19dw19@ikzubirimanteo.com>
2020-12-17 10:09:20 +0100

    Inicio sesion funcional

--- routes/web.php
+++ routes/web.php
@@ -15,12 +15,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('index');
-})->name('inicio');
+})->name('home');
 
 
 
-Route::get('lang/{lang}', 'LanguageController@swap')->name('lang.swap');
+// Route::get('lang/{lang}', 'LanguageController@swap')->name('lang.swap');
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index']);
