50065ce6a492ff5ca40986f98a76e9babf783e6e
Endika <esanchezba19dw31@ikzubirimanteo.com>
2021-01-04 11:48:45 +0100

    Añadidos campos del form de registro al array fillable

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,7 +18,11 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'apellido',
+        'dni',
         'email',
+        'foto',
+        'usuario',
         'password',
     ];
 
