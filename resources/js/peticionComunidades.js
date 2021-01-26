77bf677160c514cf26cdb26621c1b6d570b06c62
Endika <sdsd>
2021-01-26 12:14:00 +0100

    datos de API personal en los dropdown de comunidades

--- resources/js/peticionComunidades.js
+++ resources/js/peticionComunidades.js
@@ -1,21 +1,21 @@
 let peticion_http = new XMLHttpRequest();
 
-window.onload = peticion;
+window.onload = peticion, muestraContenido;
 
 function peticion() {
-  peticion_http.open('GET', "https://apiv1.geoapi.es/comunidades?type=JSON&sandbox=1", true);
+  peticion_http.open('GET', "http://localhost/comunidades.json", true);
   peticion_http.onreadystatechange = muestraContenido;
   peticion_http.send(null);
 }
 
 function muestraContenido() {
-  var desplegable = document.getElementById('selectcoms');
+  var desplegable = document.getElementById('desp');
   if(peticion_http.readyState == 4) {
     if(peticion_http.status == 200) {
       var respuesta = peticion_http.responseText;
       var json = JSON.parse(peticion_http.responseText);
-      for(var i=0; i<json.data.length; i++) {
-          desplegable.innerHTML += "<option>" + json.data[i].COM + "<option>";
+      for(var i=0; i<json.comunidades.length; i++) {
+          desplegable.innerHTML += "<option>" + json.comunidades[i].nombre + "<option>";
       }
     }
   }
